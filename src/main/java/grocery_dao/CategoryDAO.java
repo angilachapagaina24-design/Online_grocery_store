3fbf5cad76c0ce64b635f8f8cbb25264f70c7568
Nisha Magar <thapanisha441@gmail.com>
2026-05-02 11:01:54 +0545

    servlet

--- src/main/java/grocery_dao/CategoryDAO.java
+++ src/main/java/grocery_dao/CategoryDAO.java
@@ -11,33 +11,25 @@ public class CategoryDAO {
     public List<Category> getAllCategories() {
         List<Category> list = new ArrayList<>();
 
-        try {
-            Connection con = DBGroceryConfig.getConnection();
-            String sql = "SELECT * FROM category WHERE status='active'";
-            PreparedStatement ps = con.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
+        String sql = "SELECT * FROM category WHERE status='active'";
+
+        try (Connection con = DBGroceryConfig.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
 
             while (rs.next()) {
 
                 Category c = new Category();
 
-                c.setCategoryId(
-                        rs.getInt("category_id"));
-
-                c.setCategoryName(
-                        rs.getString("category_name"));
-
-                c.setDescription(
-                        rs.getString("description"));
-
-                c.setImageUrl(
-                        rs.getString("image_url"));
+                c.setCategoryId(rs.getInt("category_id"));
+                c.setCategoryName(rs.getString("category_name"));
+                c.setDescription(rs.getString("description"));
+                c.setImageUrl(rs.getString("image_url"));
 
                 list.add(c);
             }
 
         } catch (Exception e) {
-
             e.printStackTrace();
         }
 
